27ef8d5b1d4552ea6d5bf86d29b91015ffc79e4c
grady <980126441@qq.com>
2018-12-03 21:33:51 +0800

    hello 02

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Frist 
 {
 
@@ -9,4 +11,26 @@ public class Frist
 public class void Show() 
 {
 		System.out.println("Hello World!");
+}
+class Rand 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		//获取随机数
+		//Math. random(); 范围[0,1)
+		int num = (int)(Math.random()*10);
+		System.out.println("随机数为"+num);
+
+		System.out.print("请输入你的会员号:");
+		int val = input.nextInt();
+		System.out.println();
+
+		int bai = (val/100)%10;
+
+		if(num==bai)
+			System.out.println("幸运会员");
+		else
+			System.out.println("谢谢参与");
+	}
 }
